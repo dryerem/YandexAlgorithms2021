0d85df06677a22e78120ed1dd9bd150d2264e624
dryerem <73961037+dryerem@users.noreply.github.com>
2021-09-06 19:05:34 +0300

    Delete date.py

--- date.py
+++
@@ -1,7 +0,0 @@
-x, y, z = map(int, input().split())
-
-if (1 <= x <= 31) and (1 <= y <= 31) and (1970 <= z <= 2069):
-    if x > 12 or y > 12 or x == y:
-        print(1)
-    else:
-        print(0)
